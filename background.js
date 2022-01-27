5b17bfd9f5e4d0a8b405d9932c5af9c996a6d123
kong0107 <kong0107@gmail.com>
2022-01-27 22:51:36 +0800

    first version: ok for brave browser

---
+++ background.js
@@ -0,0 +1,15 @@
+const proxy = (typeof browser === "undefined" ? chrome : browser);
+
+proxy.pageAction.onClicked.addListener(tab => {
+    const match = /[?&]v=(\w{11})/.exec(tab.url);
+    if(!match) return;
+    const video_id = match[1];
+    console.debug(video_id);
+    const url = "https://yout.com/video/?url=https://youtube.com/watch?v=" + video_id;
+    proxy.tabs.update(tab.id, {url});
+});
+
+proxy.tabs.onUpdated.addListener((tab_id, _, tab) => {
+    if(/^https?\:\/\/(www|music)\.youtube\.com\/watch\?(.+\&)*v=\w{11}/.test(tab.url))
+        proxy.pageAction.show(tab_id);
+});
