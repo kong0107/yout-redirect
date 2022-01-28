aad7fb50f3f8a95906dc6891a68fbd20910b7648
kong0107 <kong0107@gmail.com>
2022-01-29 00:42:43 +0800

    fix #1 to support ID with dash

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const proxy = (typeof browser === "undefined" ? chrome : browser);
 
 proxy.pageAction.onClicked.addListener(tab => {
-    const match = /[?&]v=(\w{11})/.exec(tab.url);
+    const match = /[?&]v=([\w\-]{11})/.exec(tab.url);
     if(!match) return;
     const video_id = match[1];
     console.debug(video_id);
@@ -10,6 +10,6 @@ proxy.pageAction.onClicked.addListener(tab => {
 });
 
 proxy.tabs.onUpdated.addListener((tab_id, _, tab) => {
-    if(/^https?\:\/\/(www|music)\.youtube\.com\/watch\?(.+\&)*v=\w{11}/.test(tab.url))
+    if(/^https?\:\/\/(www|music)\.youtube\.com\/watch\?(.+&)*v=[\w\-]{11}/.test(tab.url))
         proxy.pageAction.show(tab_id);
 });
